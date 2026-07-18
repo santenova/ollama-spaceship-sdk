7c9a8686d5175bae2a8e436166432531cf632bdf
santenova <tokenfreeai@gmail.com>
2026-07-18 11:51:00 +0200

    adding index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 
 import { clientLibrary as lib } from './apis/ClientLibrary';
 
+import { ClientLibrary, clientLibrary } from './apis/ClientLibrary';
 import { config ,createClient,client} from './apis/client';
 
+export { clientLibrary as lib } from './apis/ClientLibrary';
 
-export { lib, config ,createClient,client};
+export { config ,createClient,client} from './apis/client';
+
+
+export  { lib, config ,createClient,client} from './apis/ClientLibrary';
